ff66960d73664ceef9c13ed2089e724b33e7a21a
tiwaojo <tiwaojo@gmail.com>
2023-08-27 07:48:00 +0000

    feat: init mcap vscode feature

---
+++ src/mcap/install.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -e
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt-get update..."
+            apt-get update -y
+        fi
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+check_packages curl jq ca-certificates
+
+# Check if VERSION is set, if not, set it to 'latest'
+if [ -z "${VERSION}" ]; then
+	VERSION=latest
+fi
+
+# Check if VERSION is set to 'latest', if so, get the latest release version from GitHub
+if [ "${VERSION}" = "latest" ]; then
+	versionStr=$(curl https://api.github.com/repos/foxglove/mcap/releases/latest | jq -r '.tag_name')
+	
+	# if versionStr matches 'releases/mcap-cli/' then remove 'releases/mcap-cli/' from versionStr
+	versionStr=$(echo ${versionStr} | sed 's/releases\/mcap-cli\///g')
+	echo "Latest version: ${versionStr}"
+
+else
+	versionStr=${VERSION}
+fi
+
+# Verify system architecture
+arch=$(dpkg --print-architecture)
+case "${arch}" in
+	amd64) archStr=amd64 ;;
+	arm64) archStr=arm64 ;;
+	*)
+		echo "MCAP cli does not support machine architecture '$arch'. Please use an x86-64 or ARM64 machine."
+		exit 1
+esac
+
+# Download and install MCAP cli
+echo "Downloading MCAP cli ${versionStr} for ${archStr}..."
+curl -SL "https://github.com/foxglove/mcap/releases/download/releases%2Fmcap-cli%2F${versionStr}/mcap-linux-${archStr}" -o mcap
+
+# Move the binary to /usr/local/bin
+mv mcap /usr/local/bin/mcap
+
+echo "MCAP installed!"
+
+chmod +x /usr/local/bin/mcap
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
---
+++ test/mcap/install_mcap_32.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# Checks if the mcap binary is installed and if the version matches the expected version(0.0.32).
+check "mcap version" bash -c "mcap version | grep 'v0.0.32'"
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
